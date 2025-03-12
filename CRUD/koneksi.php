726e3882956d42ba2793c638c4174a857186245f
Juuten <m.saifuddinramadhan@gmail.com>
2025-03-12 21:54:21 +0700

    menambahkan koneksi PDO

--- CRUD/koneksi.php
+++ CRUD/koneksi.php
@@ -8,3 +8,16 @@ if (!$koneksi) {
 die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+koneksi database menggunakan PDO
+<?php
+try {
+$dsn = "mysql:host=localhost;dbname=kampus";
+$username = "root";
+$password = "";
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+$pdo = new PDO($dsn, $username, $password, $options);
+echo "Koneksi berhasil!";
+} catch (PDOException $e) {
+echo "Koneksi gagal: " . $e->getMessage();
+}
+?>
